4c7cf3338cf2757cfe370dc63f6af14b4771aa3b
CharKourkoulis <cult_bob@hotmail.com>
2021-02-07 15:07:41 +0200

    Created custom Http Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { response } from 'express';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -7,9 +9,24 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  url = '/api/courses';
   constructor() { }
 
   ngOnInit() {
+
+    const http$ = new Observable(observer => {
+      fetch(this.url).then(response => {
+        return response.json();
+      })
+      .then(body => {
+        observer.next(body);
+        observer.complete();
+      })
+      .catch(err => {
+        observer.error(err);
+      })
+    });
+
   }
 
 }
